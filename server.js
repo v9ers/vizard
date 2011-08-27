672df4d29744c08a8a0ee4bb5a29d9a9dab18b0f
Mathias Buus <mathiasbuus@gmail.com>
2011-08-27 21:32:57 +0200

    updated server

--- server.js
+++ server.js
@@ -2,22 +2,30 @@ var server = require('router').create();
 var db = require('mongojs').connect('v9ers:qweqwe@staff.mongohq.com:10013/vizard', ['graph', 'series']);
 var jsonify = require('jsonify');
 var common = require('common');
+var parseURL = require('url').parse;
 
 var analyzor = function() {
 	var that = {};
 
-	var add = function(data, serie) {
+	var add = function(data, options) {
 		if (typeof data === 'number') {
 			var line = {x:[],y:[]};
 
 			that.graph = {line:line};
 			that.total = 0;
 
-			add = function(data, serie) {
+			add = function(data, options) {
 				that.total += data;
 
-				line.x.push(serie.time);
-				line.y.push(data);
+				var last = line.x.length-1;
+				var x = line.x[last];
+
+				if (!x || x + options.period < options.time) {
+					line.x.push(options.time);
+					line.y.push(data);
+				} else {
+					line.y[last] += data;
+				}
 			};
 		}
 		if (typeof data === 'boolean') {
@@ -25,7 +33,7 @@ var analyzor = function() {
 
 			that.graph = {choice:choice};
 
-			add = function(data, serie) {
+			add = function(data, options) {
 				data = data ? 'yes' : 'no';
 
 				choice[data]++;
@@ -36,30 +44,75 @@ var analyzor = function() {
 
 			that.graph = {bar:bar};
 
-			add = function(data, serie) {
+			add = function(data, options) {
 				bar.values[data] = (bar.values[data] || 0) + 1;
+			};
+
+			that.toJSON = function() {
+				var top = Object.keys(bar.values);
+				var other = 0;	
+				var result = {};
+
+				top.sort(function(a,b) {
+					return bar.values[b] - bar.values[a];
+				});
+				top.slice(0, 5).forEach(function(t) {
+					result[t] = bar.values[t];
+				});
+					
+				for (var i in bar.values) {
+					if (result[i]) {
+						continue;
+					}
+					other += bar.values[i];
+				}
+				if (other) {
+					result.Others = other;
+				}
+
+				bar.values = result;
+
+				return that;
 			};
 		}
 		if (typeof data === 'object') {
-			add = function(data, serie) {
+			add = function(data, options) {
 				
 			};
 		}
-		add(data, serie); // bootstrap
+		add(data, options); // bootstrap
 	};
 
-	that.push = function(data, serie) {
+	that.push = function(data, options) {
 		if (Array.isArray(data)) {
 			data.forEach(function(data) {
-				that.push(data, serie);
+				that.push(data, options);
 			});
 			return;
 		}
-		add(data, serie);		
+		add(data, options);		
 	};
 
 	return that;
 };
+
+var parseTime = function() {
+	var times = {secs:1, mins:60, hours:3600, days:24*3600, weeks:7*24*3600, months:30*24*3600};
+	var alias = {s:'secs', m:'mins', h:'hours', d:'days', w:'weeks'};
+	
+	return function(data) {
+		if (!data) {
+			return 0;
+		}
+		data = data.match(/(\d+)\s*(\w)/i);
+
+		if (!data) {
+			return 0;
+		}
+		return parseInt(data[1], 10) * 1000 * (times[alias[data[2]] || alias[data[2] + 's'] || data[2]] || 1);
+	};
+}();
+
 var fork = function(respond, fn) {
 	if (!fn) {
 		fn = respond;
@@ -82,21 +135,28 @@ server.post('/r/{id}', jsonify(function(request, data, respond) {
 		respond({success:true});
 	}));
 }));
+
 server.get('/r/{id}', jsonify(function(request, respond) {
 	db.series.find({id:request.params.id}, {_id:0}, fork(respond));
 }));
+
 server.get('/v/{id}/{name}?', jsonify(function(request, respond) {
+	var query = parseURL(request.url, true).query;
+	var window = Date.now() - parseTime(query.window || '1w');
+
 	common.step([
 		function(next) {
-			db.series.find({id:request.params.id}, {_id:0}, next);
+			db.series.find({id:request.params.id, time:{$gte:window}}, {_id:0}, next);
 		},
 		function(series) {
 			if (!series.length) {
-				respond(404);
+				respond(404, {});
 				return;
 			}
+
 			var name = request.params.name;
 			var result = {};
+			var period = query.period ? parseTime(query.period) : 1;
 
 			series.forEach(function(serie) {
 				var keys = name ? [name] : Object.keys(serie);
@@ -110,7 +170,7 @@ server.get('/v/{id}/{name}?', jsonify(function(request, respond) {
 					}
 					var analyse = result[key] = result[key] || analyzor(key, serie[key]);
 
-					analyse.push(serie[key], serie);					
+					analyse.push(serie[key], {time:serie.time, period:period});
 				});
 			});
 
