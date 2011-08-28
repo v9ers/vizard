98a0d4a1bdd0a9bf4e9d7f9a44764ab247c60864
Bjarke Walling <bwp@bwp.dk>
2011-08-28 04:47:36 +0200

    Working generic examples. goan

--- static/js/vizard.js
+++ static/js/vizard.js
@@ -45,10 +45,95 @@
 						}
 					},
 					series: [{
-						data: firstData.values
+						data: []
 					}]
 				});
 			});
+
+			var renderLine = function(data) {
+				var mustRedraw = false;
+				var firstPoint = 0;
+				var lastEntry = chart.series[0].data[chart.series[0].data.length-1];
+
+				for (var i=0; i<data.length; i++) {
+					if (lastEntry && lastEntry.name === data[i].x) {
+						if (lastEntry.y === data[i].y) {
+							firstPoint = i+1;
+							break;
+						} else {
+							lastEntry.update(data[i].y);
+							firstPoint = data.length;
+						}
+					}
+				}
+
+				for (var i=firstPoint; i<data.length; i++) {
+					mustRedraw = true;
+					chart.xAxis[0].categories.push(data[i].x);
+
+					if (chart.xAxis[0].categories.length > data.length) {
+						chart.series[0].addPoint({name:data[i].x, y:data[i].y}, false, true, true);
+					} else {
+						chart.series[0].addPoint({name:data[i].x, y:data[i].y}, false, false, true);
+					}
+				}
+
+				if (mustRedraw) {
+					chart.redraw();
+				}
+			};
+			
+			var renderColumn = function(data) {
+				var mustRedraw = false;
+				var series = chart.series[0];
+				var xs = chart.xAxis[0].categories;
+				var numberOfBars = xs.length;
+				var modifiedBars = [];
+
+				for (var i=0; i<data.length; i++) {
+					var x = data[i].x;
+					var y = data[i].y;
+
+					if (i >= numberOfBars) {
+						xs[i] = x;
+						series.addPoint({x:i, y:y}, false, false, true);
+						mustRedraw = true;
+					} else {
+						var bar = series.data[i];
+						if (xs[i] !== x || bar.y !== y) {
+
+							if (xs[i] !== x && x !== 'Others') {
+								bar.graphic.css({ 'fill-opacity': 0.75 });
+								modifiedBars.push(bar);
+							}
+
+							xs[i] = x;
+							bar.update(y, false, true);
+							mustRedraw = true;
+						}
+					}
+				}
+
+				for (var i=numberOfBars-1; i>=data.length; i--) {
+					series.data[i].remove(false, true);
+					mustRedraw = true;
+				}
+		
+				xs.length = data.length;
+
+				if (mustRedraw) {
+					chart.redraw();
+
+					(function(modifiedBars) {
+						setTimeout(function() {
+							modifiedBars.forEach(function(bar) {
+								bar.graphic.css({'fill-opacity': 1});
+							});
+						}, 750);
+					}(modifiedBars));
+				}
+			};
+			
 			
 			return {
 				set: function(data) {
@@ -57,53 +142,12 @@
 						return;
 					}
 
-					var mustRedraw = false;
-					var series = chart.series[0];
-					var xs = chart.xAxis[0].categories;
-					var numberOfBars = xs.length;
-					var modifiedBars = [];
-
-					for (var i=0; i<data.length; i++) {
-						var x = data[i].x;
-						var y = data[i].y;
-						
-						if (i >= numberOfBars) {
-							xs.push(x);
-							series.addPoint(y, false, false, true);
-							mustRedraw = true;
-						} else {
-							var bar = series.data[i];
-							if (xs[i] !== x || bar.y !== y) {
-
-								if (xs[i] !== x && x !== 'Others') {
-									bar.graphic.css({ 'fill-opacity': 0.75 });
-									modifiedBars.push(bar);
-								}
-
-								xs[i] = x;
-								bar.update(y, false, true);
-								mustRedraw = true;
-							}
-						}
+					if (type === 'line') {
+						renderLine(data);
 					}
 
-					for (var i=data.length; i<numberOfBars; i++) {
-						series.data[i].remove(false, true);
-						mustRedraw = true;
-					}
-					
-					xs.length = data.length;
-
-					if (mustRedraw) {
-						chart.redraw();
-						
-						(function(modifiedBars) {
-							setTimeout(function() {
-								modifiedBars.forEach(function(bar) {
-									bar.graphic.css({'fill-opacity': 1});
-								});
-							}, 750);
-						}(modifiedBars));
+					if (type === 'column') {
+						renderColumn(data);
 					}
 				}
 			};
